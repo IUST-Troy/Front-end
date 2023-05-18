f6c133d1ef6829133c32046b81a1ff1b346b6de0
Pooria <pooria_rahimi@comp.iust.ac.ir>
2023-05-18 21:47:58 +0330

    [fix] : change design

--- src/Components/Profile-tourleader/profile-tourleader.js
+++ src/Components/Profile-tourleader/profile-tourleader.js
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-  Button,
-  Card,
-} from "flowbite-react";
+import { Button, Card } from "flowbite-react";
 import imgValue from "../../Static/myl.png";
 import "../../Styles/ProfilePage/ProfilePage.scss";
 import { CiLocationOn } from "react-icons/ci";
@@ -42,6 +39,57 @@ const ProfiletourleaderPage = () => {
                     World Medical Relief
                   </span>
                 </div>
+                <div class="flex flex-col items-center gap-2">
+                    <span class="flex items-center gap-4 text-sm rounded text-slate-500">
+                      <span class="flex gap-1 text-amber-400" role="img" aria-label="Rating: 4 out of 5 stars">
+                        <span aria-hidden="true">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"clip-rule="evenodd"/>
+                          </svg>
+                        </span>
+                        <span aria-hidden="true">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"clip-rule="evenodd"/>
+                          </svg>
+                        </span>
+                        <span aria-hidden="true">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path
+                              fill-rule="evenodd"
+                              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                        <span aria-hidden="true">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                        <span aria-hidden="true">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                            />
+                          </svg>
+                        </span>
+                      </span>
+                    </span>
+                  </div>
                 <div class="border-t text-gray-200 text-center">
                   <div class="flex flex-warp justify-center">
                     <div class="mt-5 w-auto h-auto lg:w-9/12 px-4">
@@ -56,8 +104,12 @@ const ProfiletourleaderPage = () => {
                 <div class="border-t text-gray-200 text-center">
                   <div class="flex flex-wrap justify-center">
                     <div class="mt-5  w-auto h-auto lg:w-9/12 px-auto">
-                      <p class="text-lg leading-relaxed text-gray-700">example@gmail.com</p>
-                      <p class="text-lg leading-relaxed text-gray-700">+989177521289</p>
+                      <p class="text-lg leading-relaxed text-gray-700">
+                        example@gmail.com
+                      </p>
+                      <p class="text-lg leading-relaxed text-gray-700">
+                        +989177521289
+                      </p>
                     </div>
                   </div>
                 </div>
